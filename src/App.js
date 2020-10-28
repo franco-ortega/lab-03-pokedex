f23ef30db994f2a6dbb87732bcb4625d14ca934a
Franco Ortega <francopdx@gmail.com>
2020-10-28 16:06:21 -0700

    create js files

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Header from './Header.js';
+import Nav from './Nav.js';
+import ImageList from './ImageList.js';
+//create ImageItem in ImageItem.js
+import Search from './Search.js';
+import Sort from './Sort.js';
+import data from './Data.js';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        App.js Page
+        <Header />
+        <Nav />
+        <ImageList pokemon={data} />
+        <Search />
+        <Sort />
+      </div>
+    )
+  }
+}
