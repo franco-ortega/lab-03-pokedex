854dd0ef6ea3370268e15c67ae5d1d0d1901a7bb
Franco Ortega <francopdx@gmail.com>
2020-10-28 16:30:31 -0700

    initialize eventlisteners

--- src/App.js
+++ src/App.js
@@ -7,6 +7,31 @@ import Sort from './Sort.js';
 import data from './Data.js';
 
 export default class App extends Component {
+
+  state = {
+    filterName: '',
+    sortAlphabetical: '',
+    sortType: '',
+  }
+
+  handleNameChange = (e) => {
+    this.setState({
+      filterName: e.target.value
+    });
+  }
+
+  handleAlphabeticalChange = (e) => {
+    this.setState({
+      sortAlphabetical: e.target.value
+    });
+  }
+
+  handleTypeChange = (e) => {
+    this.setState({
+      sortType: e.target.value
+    });
+  }
+
   render() {
     return (
       <div>
