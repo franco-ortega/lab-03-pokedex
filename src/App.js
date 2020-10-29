8e313da7644c287e4e5658612fed2b5b224ed6ae
Franco Ortega <francopdx@gmail.com>
2020-10-28 17:11:58 -0700

    add text to header and initial stylings

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './App.css';
 import Header from './Header.js';
 import Nav from './Nav.js';
 import ImageList from './ImageList.js';
--- src/Header.js
+++ src/Header.js
@@ -5,6 +5,12 @@ export default class Header extends Component {
         return (
             <div>
                 Header
+                <div className='title-div'>
+                    Welcome to the Pokedex!!!
+                </div>
+                <div className='intro-div'>
+                    Search for all the Pokemon in many different ways! Yeeeaaahhhh!!!!!
+                </div>
             </div>
         )
     }
