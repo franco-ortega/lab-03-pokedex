50cf60ee1f9d5cae275267f0e551e006b5b54467
Franco Ortega <francopdx@gmail.com>
2020-10-28 17:23:27 -0700

    add text, input, and button to search.js and added styling

--- src/Search.js
+++ src/Search.js
@@ -3,8 +3,16 @@ import React, { Component } from 'react';
 export default class Search extends Component {
     render() {
         return (
-            <div>
-                Search
+            <div className='search-div'>
+                <div className='search-text-div'>
+                    Search for Pokemon here:
+                </div>
+                <div className='search-input-div'>
+                    <input />
+                </div>
+                <div className='search-button-div'>
+                    <button>Press Here to Search</button>
+                </div>
             </div>
         )
     }
