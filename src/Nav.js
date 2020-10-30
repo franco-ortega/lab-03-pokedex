52b3804173b6bcd4dd279e3bbf0f6d7cefcbefa4
Franco Ortega <francopdx@gmail.com>
2020-10-29 22:31:40 -0700

    start lab 04

--- src/Nav.js
+++ src/Nav.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Nav extends Component {
     render() {
         return (
             <div className='nav-div'>
-                Nav Links
+                <div>Nav Links</div>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                </ul>
+                <ul>
+                    <li><Link to="/list">All Pokemon</Link></li>
+                </ul>
+
+
+
             </div>
         )
     }
